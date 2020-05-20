39d1c0887c9dba70efd0af5926cd4a2be1ca3bc0
Jiezhi.Xiao <jzxiao@thoughtworks.com>
2020-05-20 21:59:04 +0800

    refactor: move update good method to more related class Goods

--- src/main/java/cn/xpbootcamp/gilded_rose/GildedRose.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/GildedRose.java
@@ -9,54 +9,7 @@ class GildedRose {
 
     public void updateQuality() {
         for (Goods good : goods) {
-            if (!good.getName().equals("Aged Brie")
-                    && !good.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (good.isReachQualityBottomLimit()) {
-                    if (!good.getName().equals("Sulfuras, Hand of Ragnaros")) {
-                        good.setQuality(good.getQuality() - 1);
-                    }
-                }
-            } else {
-                if (good.isReachQualityTopLimit()) {
-                    good.setQuality(good.getQuality() + 1);
-
-                    if (good.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (good.getSellIn() < 11) {
-                            if (good.isReachQualityTopLimit()) {
-                                good.setQuality(good.getQuality() + 1);
-                            }
-                        }
-
-                        if (good.getSellIn() < 6) {
-                            if (good.isReachQualityTopLimit()) {
-                                good.setQuality(good.getQuality() + 1);
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!good.getName().equals("Sulfuras, Hand of Ragnaros")) {
-                good.decreaseSellIn();
-            }
-
-            if (good.getSellIn() < 0) {
-                if (!good.getName().equals("Aged Brie")) {
-                    if (!good.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (good.isReachQualityBottomLimit()) {
-                            if (!good.getName().equals("Sulfuras, Hand of Ragnaros")) {
-                                good.setQuality(good.getQuality() - 1);
-                            }
-                        }
-                    } else {
-                        good.setQuality(0);
-                    }
-                } else {
-                    if (good.isReachQualityTopLimit()) {
-                        good.setQuality(good.getQuality() + 1);
-                    }
-                }
-            }
+            good.updateByDay();
         }
     }
 
--- src/main/java/cn/xpbootcamp/gilded_rose/Goods.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/Goods.java
@@ -51,4 +51,55 @@ public class Goods {
     boolean isReachQualityBottomLimit() {
         return getQuality() > 0;
     }
+
+    void updateByDay() {
+        if (!getName().equals("Aged Brie")
+                && !getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (isReachQualityBottomLimit()) {
+                if (!getName().equals("Sulfuras, Hand of Ragnaros")) {
+                    setQuality(getQuality() - 1);
+                }
+            }
+        } else {
+            if (isReachQualityTopLimit()) {
+                setQuality(getQuality() + 1);
+
+                if (getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (getSellIn() < 11) {
+                        if (isReachQualityTopLimit()) {
+                            setQuality(getQuality() + 1);
+                        }
+                    }
+
+                    if (getSellIn() < 6) {
+                        if (isReachQualityTopLimit()) {
+                            setQuality(getQuality() + 1);
+                        }
+                    }
+                }
+            }
+        }
+
+        if (!getName().equals("Sulfuras, Hand of Ragnaros")) {
+            decreaseSellIn();
+        }
+
+        if (getSellIn() < 0) {
+            if (!getName().equals("Aged Brie")) {
+                if (!getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (isReachQualityBottomLimit()) {
+                        if (!getName().equals("Sulfuras, Hand of Ragnaros")) {
+                            setQuality(getQuality() - 1);
+                        }
+                    }
+                } else {
+                    setQuality(0);
+                }
+            } else {
+                if (isReachQualityTopLimit()) {
+                    setQuality(getQuality() + 1);
+                }
+            }
+        }
+    }
 }
